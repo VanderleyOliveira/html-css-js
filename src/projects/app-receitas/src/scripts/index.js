dd058599bed9478111d32e0be8fd2bd1d444cfce
VanderleyOliveira <vanderleyoliv21@gmail.com>
2024-12-23 16:01:14 -0300

    project: app-receitas final

--- src/projects/app-receitas/src/scripts/index.js
+++ src/projects/app-receitas/src/scripts/index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.search-form')
 const recipeList = document.querySelector('.recipe-list')
+const recipeDetails = document.querySelector('.recipe-details')
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -9,10 +10,16 @@ form.addEventListener('submit', (event) => {
 })
 
 async function searchRecipes(ingredient) {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
-    const data = await response.json();
+    recipeList.innerHTML = `<p>Carregando receitas...</p>`
 
-    showRecipes(data.meals)
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+        const data = await response.json();
+
+        showRecipes(data.meals)
+    } catch (err) {
+        recipeList.innerHTML = `<p>Nenhuma receita encontrada</p>`
+    }
 }
 
 function showRecipes(recipes) {
@@ -25,7 +32,33 @@ function showRecipes(recipes) {
     ).join('')
 }
 
-async function getRecipesDetails(id){
-    const response = await fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+async function getRecipesDetails(id) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     const data = await response.json();
+
+    const recipe = data.meals[0]
+
+    let ingredients = ''
+
+    for (let i = 1; i <= 20; i++) {
+        if (recipe[`strIngredient${i}`]) {
+            ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}</li>`
+
+        } else {
+            break
+        }
+    }
+
+    recipeDetails.innerHTML = `
+        <h2>${recipe.strMeal}</h2>
+        <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+        <h3>Categoria: ${recipe.strCategory}</h3>
+        <h3>Origem: ${recipe.strArea}</h3>
+        <h3>Ingredientes:</h3>
+        <ul>${ingredients}</ul>
+        <h3>Instruções:</h3>
+        <p>${recipe.strInstructions}</p>
+        <p>Tags: ${recipe.strTags}</p>
+        <p>Vídeo: <a href="${recipe.strYoutube}" target="_blank">Assista no Youtube</a></p>
+    `
 }
